f5b648a3b3ebd6f06fab2a381dc82e659b610124
Felipe Antunes <felipeantunes542@gmail.com>
2025-04-29 07:48:54 -0300

    continue header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,11 @@ import { PiCat } from "react-icons/pi";
 const Header = () => {
   return (
     <div className="fixed top-0 left-0">
-      <PiCat />
+      <div className="px-4 pt-4 w-full max-w-[1200px] mx-auto flex justify-between">
+        <div className="p-4 bg-black">
+          <PiCat className="text-3xl" />
+        </div>
+      </div>
     </div>
   );
 };

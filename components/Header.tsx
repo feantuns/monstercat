59d51c267a855c2e153a76fed19a8f9cc3020ec2
Felipe Antunes <felipeantunes542@gmail.com>
2025-04-30 07:31:55 -0300

    align fixed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import { PiCat } from "react-icons/pi";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 left-0">
-      <div className="px-4 pt-4 w-full max-w-[1200px] mx-auto flex justify-between">
+    <div className="w-full fixed top-0 left-0">
+      <div className="px-[9vw] pt-6 flex w-full max-w-[1600px] mx-auto justify-between">
         <div className="p-4 bg-black">
           <PiCat className="text-3xl" />
         </div>

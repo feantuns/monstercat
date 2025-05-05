84d038e587ff67cf0ceaf7c8dc2926fc1f86d5be
Felipe Antunes <felipeantunes542@gmail.com>
2025-05-05 07:39:05 -0300

    add links to header icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,20 +13,36 @@ const Header = () => {
     <div className="w-full fixed top-0 left-0">
       <div className="px-[9vw] pt-6 flex items-start w-full max-w-[1600px] mx-auto justify-between">
         <div className="p-4 bg-black">
-          <PiCat className="text-3xl" />
+          <a href="#">
+            <PiCat className="text-3xl" />
+          </a>
         </div>
 
-        <div className="flex flex-col gap-10 justify-center items-center">
-          <AiOutlineMenu className="text-3xl mt-4" />
+        <div className="flex flex-col gap-14 items-end">
+          <AiOutlineMenu className="text-2xl mt-4 cursor-pointer" />
 
-          <div className="flex flex-col gap-6 justify-center items-center">
-            <PiInstagramLogo className="text-xl" />
-            <FaTiktok className="text-xl" />
-            <PiXLogoLight className="text-xl" />
-            <PiTwitchLogoBold className="text-xl" />
-            <FaFacebook className="text-xl" />
-            <IoLogoDiscord className="text-xl" />
-            <PiPlayCircleFill className="text-xl" />
+          <div className="flex flex-col gap-5 justify-center items-center">
+            <a href="#">
+              <PiInstagramLogo className="text-[1.2rem]" />
+            </a>
+            <a href="#">
+              <FaTiktok className="text-[1.2rem]" />
+            </a>
+            <a href="#">
+              <PiXLogoLight className="text-[1.2rem]" />
+            </a>
+            <a href="#">
+              <PiTwitchLogoBold className="text-[1.2rem]" />
+            </a>
+            <a href="#">
+              <FaFacebook className="text-[1.2rem]" />
+            </a>
+            <a href="#">
+              <IoLogoDiscord className="text-[1.2rem]" />
+            </a>
+            <a href="#">
+              <PiPlayCircleFill className="text-[1.2rem]" />
+            </a>
           </div>
         </div>
       </div>

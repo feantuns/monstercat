855efc7d40cccb877403880387a7381152f56921
Felipe Antunes <felipeantunes542@gmail.com>
2025-05-22 06:33:12 -0300

    fix: button border

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,13 +9,13 @@ const Button = ({
 }) => {
   console.log(color);
   const buttonStyles = {
-    contained: `bg-[var(--bg)] border-2 border-transparent text-white`,
-    outlined: `border-2 border-white text-white`,
+    contained: `bg-[var(--bg)] border-1 border-transparent text-white`,
+    outlined: `border-1 border-white text-white`,
   };
   return (
     <button
       style={{ "--bg": color } as any}
-      className={`${buttonStyles[variant]} cursor-pointer pl-7 pr-8 py-2 text-xl font-normal transition-all duration-200 hover:bg-white hover:text-black focus:outline-none flex items-center justify-center gap-2 tracking-[1px]`}
+      className={`${buttonStyles[variant]} cursor-pointer pl-7 pr-8 py-3 text-xl font-normal transition-all duration-200 hover:bg-white hover:text-black focus:outline-none flex items-center justify-center gap-2 tracking-[1px]`}
     >
       {children}
     </button>

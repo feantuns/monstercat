8d8d1c87189183c2b2270feb55d3e80b76e2066b
Felipe Antunes <felipeantunes542@gmail.com>
2025-06-02 07:45:55 -0300

    add track list title

--- components/TrackList.tsx
+++ components/TrackList.tsx
@@ -1,5 +1,12 @@
+import Container from "./Container";
+import SectionTitle from "./SectionTitle";
+
 const TrackList = () => {
-  return null;
+  return (
+    <Container>
+      <SectionTitle className="mt-12">Track list</SectionTitle>
+    </Container>
+  );
 };
 
 export default TrackList;

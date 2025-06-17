6f09d5645c0fccdc706fb5808c3e9472bb4f99b9
Felipe Antunes <felipeantunes542@gmail.com>
2025-06-17 07:02:13 -0300

    add music video

--- components/MusicVideo.tsx
+++ components/MusicVideo.tsx
@@ -4,7 +4,17 @@ import SectionTitle from "./SectionTitle";
 const MusicVideo = () => {
   return (
     <Container>
-      <SectionTitle className="mt-8 mb-6">Music video</SectionTitle>
+      <SectionTitle className="mt-8 mb-8">Music video</SectionTitle>
+      <iframe
+        width="100%"
+        height="500"
+        src="https://www.youtube.com/embed/MrY-XZ45Rfk?si=xgCx4_tTRPyVxhDD"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </Container>
   );
 };

c97f9505b0cb17dc0f25b34d76da969fb1b0a633
Felipe Antunes <felipeantunes542@gmail.com>
2025-06-20 06:45:05 -0300

    continue footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,30 @@
 import Container from "./Container";
+import SectionTitle from "./SectionTitle";
 
 const Footer = () => {
   return (
     <footer className="py-20">
-      <Container>Footer</Container>
+      <Container>
+        <div className="py-10 border-t-1 border-b-1 border-white flex gap-x-6 flex-wrap">
+          <div className="flex-1">
+            <a href="#">
+              <SectionTitle>About Monstercat</SectionTitle>
+            </a>
+            <a href="#">
+              <SectionTitle>Contact us</SectionTitle>
+            </a>
+            <a href="#">
+              <SectionTitle>Carreers</SectionTitle>
+            </a>
+            <a href="#">
+              <SectionTitle>News</SectionTitle>
+            </a>
+            <a href="#">
+              <SectionTitle>Press</SectionTitle>
+            </a>
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 };

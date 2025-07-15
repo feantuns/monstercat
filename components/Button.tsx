816bdde69c0ec8bedb083852cfe25440f35595e9
Felipe Antunes <felipeantunes542@gmail.com>
2025-07-15 10:59:12 -0300

    responsive button

--- components/Button.tsx
+++ components/Button.tsx
@@ -17,7 +17,7 @@ const Button = ({
   };
   return (
     <button
-      className={`${buttonStyles[variant]} cursor-pointer pl-7 pr-8 py-3 text-xl font-normal transition-all duration-200 hover:bg-white hover:text-black focus:outline-none flex items-center justify-center gap-2 tracking-[1px]`}
+      className={`${buttonStyles[variant]} cursor-pointer pl-6 pr-7 py-2 md:pl-7 md:pr-8 md:py-3 text-base md:text-xl font-normal transition-all duration-200 hover:bg-white hover:text-black focus:outline-none flex items-center justify-center gap-2 tracking-[1px]`}
       {...props}
       style={{ "--bg": color, ...props?.style } as any}
     >

451f6c397a0c30f5905fd144c545969c6c553ef5
Felipe Antunes <felipeantunes542@gmail.com>
2025-07-17 09:41:06 -0300

    add social icons header on sm and md

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,16 +10,18 @@ import { PiPlayCircleFill } from "react-icons/pi";
 
 const Header = () => {
   return (
-    <div className="w-full fixed top-0 left-0 z-3 md:z-0">
-      <div className="hidden md:block">
+    <div className="w-full fixed top-0 left-0 z-3 lg:z-0">
+      <div className="hidden lg:block">
         <DesktopHeader />
       </div>
-      <div className="md:hidden">
+      <div className="lg:hidden">
         <MobileHeader />
       </div>
     </div>
   );
 };
+
+export default Header;
 
 const DesktopHeader = () => (
   <div className="px-[32px] pt-6 flex items-start w-full max-w-[1260px] mx-auto justify-between">
@@ -33,27 +35,7 @@ const DesktopHeader = () => (
       <AiOutlineMenu className="text-2xl mt-4 cursor-pointer" />
 
       <div className="flex flex-col gap-5 justify-center items-center">
-        <a href="#">
-          <PiInstagramLogo className="text-[1.2rem]" />
-        </a>
-        <a href="#">
-          <FaTiktok className="text-[1.2rem]" />
-        </a>
-        <a href="#">
-          <PiXLogoLight className="text-[1.2rem]" />
-        </a>
-        <a href="#">
-          <PiTwitchLogoBold className="text-[1.2rem]" />
-        </a>
-        <a href="#">
-          <FaFacebook className="text-[1.2rem]" />
-        </a>
-        <a href="#">
-          <IoLogoDiscord className="text-[1.2rem]" />
-        </a>
-        <a href="#">
-          <PiPlayCircleFill className="text-[1.2rem]" />
-        </a>
+        <SocialIcons />
       </div>
     </div>
   </div>
@@ -63,8 +45,12 @@ const MobileHeader = () => (
   <div className="bg-black/38 relative px-4 py-2 flex items-center justify-between w-full max-w-[1260px] mx-auto">
     <div className="p-2 bg-black relative z-1">
       <a href="#">
-        <PiCat className="text-2xl" />
+        <PiCat className="text-2xl sm:text-3xl" />
       </a>
+    </div>
+
+    <div className="hidden sm:flex flex-row gap-5 justify-center items-center relative z-1">
+      <SocialIcons />
     </div>
 
     <AiOutlineMenu className="text-2xl cursor-pointer relative z-1" />
@@ -73,4 +59,28 @@ const MobileHeader = () => (
   </div>
 );
 
-export default Header;
+const SocialIcons = () => (
+  <>
+    <a href="#">
+      <PiInstagramLogo className="text-[1.2rem]" />
+    </a>
+    <a href="#">
+      <FaTiktok className="text-[1.2rem]" />
+    </a>
+    <a href="#">
+      <PiXLogoLight className="text-[1.2rem]" />
+    </a>
+    <a href="#">
+      <PiTwitchLogoBold className="text-[1.2rem]" />
+    </a>
+    <a href="#">
+      <FaFacebook className="text-[1.2rem]" />
+    </a>
+    <a href="#">
+      <IoLogoDiscord className="text-[1.2rem]" />
+    </a>
+    <a href="#">
+      <PiPlayCircleFill className="text-[1.2rem]" />
+    </a>
+  </>
+);

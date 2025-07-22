2aeb858c1101401e72bbe8a1f628a176936526ad
Felipe Antunes <felipeantunes542@gmail.com>
2025-07-22 07:12:14 -0300

    fix: zIndex problem sidemenu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import { PiCat } from "react-icons/pi";
 import { PiInstagramLogo } from "react-icons/pi";
 import { FaTiktok } from "react-icons/fa";
@@ -6,24 +8,33 @@ import { PiTwitchLogoBold } from "react-icons/pi";
 import { FaFacebook } from "react-icons/fa";
 import { IoLogoDiscord } from "react-icons/io5";
 import { PiPlayCircleFill } from "react-icons/pi";
-import SideMenu from "./SideMenu";
+import SideMenu, { SideMenuBar } from "./SideMenu";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenMenu = () => setOpen(true);
+
   return (
-    <div className="w-full fixed top-0 left-0 z-3 lg:z-1">
+    <div
+      className="w-full fixed top-0 left-0 z-3 lg:z-1"
+      style={{ zIndex: open ? 40 : undefined }}
+    >
       <div className="hidden lg:block">
-        <DesktopHeader />
+        <DesktopHeader handleOpenMenu={handleOpenMenu} />
       </div>
       <div className="lg:hidden">
-        <MobileHeader />
+        <MobileHeader handleOpenMenu={handleOpenMenu} />
       </div>
+
+      <SideMenuBar open={open} setOpen={setOpen} />
     </div>
   );
 };
 
 export default Header;
 
-const DesktopHeader = () => (
+const DesktopHeader = ({ handleOpenMenu }) => (
   <div className="px-[32px] pt-6 flex items-start w-full max-w-[1260px] mx-auto justify-between">
     <div className="p-4 bg-black">
       <a href="#">
@@ -32,7 +43,7 @@ const DesktopHeader = () => (
     </div>
 
     <div className="flex flex-col gap-14 items-end">
-      <SideMenu style={{ marginTop: "1rem" }} />
+      <SideMenu style={{ marginTop: "1rem" }} onClick={handleOpenMenu} />
 
       <div className="flex flex-col gap-5 justify-center items-center">
         <SocialIcons />
@@ -41,7 +52,7 @@ const DesktopHeader = () => (
   </div>
 );
 
-const MobileHeader = () => (
+const MobileHeader = ({ handleOpenMenu }) => (
   <div className="bg-black/38 relative px-4 py-2 flex items-center justify-between w-full max-w-[1260px] mx-auto">
     <div className="p-2 bg-black relative z-1">
       <a href="#">
@@ -53,7 +64,7 @@ const MobileHeader = () => (
       <SocialIcons />
     </div>
 
-    <SideMenu />
+    <SideMenu onClick={handleOpenMenu} />
 
     <div className="backdrop-blur-[8px] bg-black/38 absolute top-0 left-0 right-0 w-full h-full z-0"></div>
   </div>
--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -6,36 +6,35 @@ import { IoMdClose } from "react-icons/io";
 import { useState } from "react";
 
 const SideMenu = props => {
-  const [open, setOpen] = useState(false);
-
   return (
-    <>
-      <AiOutlineMenu
-        className="text-2xl cursor-pointer relative z-1"
-        onClick={() => setOpen(true)}
-        {...props}
-      />
+    <AiOutlineMenu
+      className="text-2xl cursor-pointer relative z-1"
+      {...props}
+    />
+  );
+};
 
-      <div
-        className={clsx(
-          "fixed p-5 bg-black top-0 right-0 h-full w-[450px] max-w-full transform transition-transform duration-300 z-40",
-          open ? "translate-x-0" : "translate-x-full"
-        )}
-      >
-        <div className="flex items-center justify-between">
-          <a href="#" className="flex items-center gap-2">
-            <PiCat className="text-3xl" />
+export const SideMenuBar = ({ open, setOpen }) => {
+  return (
+    <div
+      className={clsx(
+        "fixed p-5 bg-black top-0 right-0 h-full w-[450px] max-w-full transform transition-transform duration-300 z-40",
+        open ? "translate-x-0" : "translate-x-full"
+      )}
+    >
+      <div className="flex items-center justify-between">
+        <a href="#" className="flex items-center gap-2">
+          <PiCat className="text-3xl" />
 
-            <span className="text-3xl font-semibold">monstercat</span>
-          </a>
+          <span className="text-3xl font-semibold">monstercat</span>
+        </a>
 
-          <IoMdClose
-            className="text-2xl cursor-pointer"
-            onClick={() => setOpen(false)}
-          />
-        </div>
+        <IoMdClose
+          className="text-2xl cursor-pointer"
+          onClick={() => setOpen(false)}
+        />
       </div>
-    </>
+    </div>
   );
 };
 

767d361e504020732ff7a56778f433299a156099
Felipe Antunes <felipeantunes542@gmail.com>
2025-09-12 06:41:08 -0300

    use react-lite-youtube-embed

--- components/MusicVideo.tsx
+++ components/MusicVideo.tsx
@@ -1,3 +1,5 @@
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 import Container from "./Container";
 import SectionTitle from "./SectionTitle";
 
@@ -5,16 +7,7 @@ const MusicVideo = () => {
   return (
     <Container>
       <SectionTitle className="mt-8 mb-8">Music video</SectionTitle>
-      <iframe
-        width="100%"
-        className="h-[280px] md:h-[400px] lg:h-[500px]"
-        src="https://www.youtube.com/embed/MrY-XZ45Rfk?si=xgCx4_tTRPyVxhDD"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+      <LiteYouTubeEmbed id="MrY-XZ45Rfk" title="YouTube video" />
     </Container>
   );
 };
